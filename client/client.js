f30f13b146cb6d7a76b7640c911aa7542140f9e1
John Shields <john@agilitec-inc.com>
2015-01-17 19:19:14 -0600

    Updated to display plants and selected house

--- client/client.js
+++ client/client.js
@@ -1,14 +1,40 @@
 Template.selectHouse.helpers({
 	houseNameId: function houseNameId() {
-		return Houses.find({}, {});
+		return Houses.find({}, {fields: {name: 1, _id: 1} });
 	},
 	isSelected: function isSelected() {
 		return Session.equals('selectedHouse', this._id) ? 'selected' : '';
 	}
 });
 
-Template.selectHouse.events = {
-	'change #selectedHouse': function selectedHouseChange(evt) {
+Template.selectHouse.events({
+	'change #selectHouse': function selectedHouseChange(evt) {
 		Session.set("selectedHouse", evt.currentTarget.value);
 	}
-};
+});
+
+Template.showHouse.helpers({
+	house: function house() {
+		return Houses.findOne({_id: Session.get("selectedHouse")});
+	}
+});
+
+Template.plantDetails.events({
+		var plantId = $(evt.currentTarget).attr('data-id');
+		Session.set(plantId, true);
+
+		var lastVisit = new Date();
+		Houses.update({_id: Session.get("selectedHouse")}, {$set: {lastVisit: lastVisit }});
+	}
+});
+
+Template.plantDetails.helpers({
+	isWatered: function isWatered() {
+		var plantId = Session.get("selectedHouse") + '-' + this.color;
+		return Session.get(plantId) ? 'disabled' : '';
+	}
+});
+
+Tracker.autorun(function logConsole() {
+	console.log("The selectedHouse ID is: " + Session.get("selectedHouse"));
+});
